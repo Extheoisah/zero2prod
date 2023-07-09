9fb1691783d9dd57c104879d2c174d2a7cde7384
Theophilus <isahtheophilus@gmail.com>
2023-07-09 01:48:04 +0100

    feat: add basic health check enpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+async fn healthz() -> impl Responder {
+    HttpResponse::Ok().body("App is running!")
+}
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().route("/health_check", web::get().to(healthz)))
+        .bind("127.0.0.1:8000")?
+        .run()
+        .await
 }
